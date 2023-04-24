c4354c803053286f6aa786a2b4d92e6b3a79778c
Ezra <81970147+ezraRubio@users.noreply.github.com>
2023-04-24 14:12:46 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://ill-pear-firefly-tutu.cyclic.app/" });
+const API = axios.create({ baseURL: "http://pet-agency.us-east-1.elasticbeanstalk.com" });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("loggedUser");
   if (token) req.headers.Authorization = `Bearer ${token}`;
